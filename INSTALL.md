6d82b37d29c6e131214b0b0200caf9df6a4476fe
Lisias T <me@lisias.net>
2023-07-11 23:44:35 -0300

    Creating projects for dedicated DLL files for KSP < 1.8 and for KSP >= 1.8. Manual installation will need some extra care.

--- INSTALL.md
+++ INSTALL.md
@@ -17,6 +17,11 @@ To install, place the GameData folder inside your Kerbal Space Program folder. O
 * Extract the package's `PluginData` folder (if available) into your KSP's root, taking precautions to do not overwrite your custom settings if this is not what you want to.
 	+ `<PACKAGE>/PluginData` --> `<KSP_ROOT>/`
 	+ You can safely ignore this step if you already had installed it previously and didn't deleted your custom configurable files.
+* This one is unorthodox: depending of what KSP you are using, you will need to remove one of the following DLL files from your GameData:
+	+ If you are using KSP until 1.7.3:
+		- delete `<KSP_ROOT>/GameData/net.lisias.ksp/AtmosphereAutopilot/Plugins/AtmosphereAutopilot180.dll`
+	+ If you are using KSP from 1.8.0 to the newest:
+		- delete `<KSP_ROOT>/GameData/net.lisias.ksp/AtmosphereAutopilot/Plugins/AtmosphereAutopilot.dll`
 
 The following file layout must be present after installation:
 
@@ -28,7 +33,8 @@ The following file layout must be present after installation:
 				[Plugins]
 					[PluginData]
 						...
-					AtmosphereAutopilot.dll
+					AtmosphereAutopilot.dll		-- Only for KSP < 1.8.0
+					AtmosphereAutopilot180.dll -- Only for KSP >= 1.8.0
 					...
 				CHANGE_LOG.md
 				LICENSE
---
+++ Source/AtmosphereAutopilot/SyncModuleControlSurface130.cs
@@ -0,0 +1,109 @@
+﻿/*
+	This file is part of Atmosphere Autopilot /L Unleashed
+		© 2018-2023 Lisias T : http://lisias.net <support@lisias.net>
+		© 2015-2020 Baranin Alexander aka Boris-Barboris
+
+	Atmosphere Autopilot /L Unleashed is licensed as follows:
+		* GPL 3.0 : https://www.gnu.org/licenses/gpl-3.0.txt
+
+	Atmosphere Autopilot /L Unleashed is free software: you can redistribute
+	it and/or modify it under the terms of the GNU General Public License as
+	published by the Free Software Foundation, either version 3 of the License,
+	or (at your option) any later version.
+
+	Atmosphere Autopilot /L Unleashed is distributed in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	You should have received a copy of the GNU General Public License 3.0 along
+	with Atmosphere Autopilot /L Unleashed. If not, see <https://www.gnu.org/licenses/>.
+
+*/
+using System;
+using UnityEngine;
+
+namespace AtmosphereAutopilot
+{
+    public partial class SyncModuleControlSurface
+    {
+        protected override void CtrlSurfaceUpdate(Vector3 vel)
+        {
+            if (null == vessel.transform) return;
+
+            this.alwaysRecomputeLift = true;
+
+            Vector3 world_com = vessel.CoM;
+            float pitch_input = ignorePitch ? 0.0f : vessel.ctrlState.pitch;
+            float roll_input = ignoreRoll ? 0.0f : vessel.ctrlState.roll;
+            float yaw_input = ignoreYaw ? 0.0f : vessel.ctrlState.yaw;
+
+            if (base.vessel.atmDensity == 0.0)
+                pitch_input = roll_input = yaw_input = 0.0f;
+
+            float spd_factor = TimeWarp.fixedDeltaTime * CSURF_SPD;
+            float fwd_airstream_factor = Mathf.Sign(Vector3.Dot(vessel.ReferenceTransform.up, vessel.srf_velocity) + 0.1f);
+            float exp_spd_factor = actuatorSpeed / actuatorSpeedNormScale * TimeWarp.fixedDeltaTime;
+
+            if (deploy)
+            {
+                float target = deployInvert ? 1.0f : -1.0f;
+                if (usesMirrorDeploy && mirrorDeploy) target *= -1.0f;
+                if (!ignorePitch) prev_pitch_normdeflection = target;
+                if (!ignoreRoll) prev_roll_normdeflection = target;
+                if (!ignoreYaw) prev_yaw_normdeflection = target;
+                was_deployed = true;
+                deflection = action = action + Common.Clampf(target - action, spd_factor);
+                ctrlSurface.localRotation = Quaternion.AngleAxis(deflection * ctrlSurfaceRange * 0.01f * authorityLimiter, Vector3.right) * neutral;
+                return;
+            }
+
+            if (ignorePitch)
+                prev_pitch_normdeflection = 0.0f;
+            else
+            {
+                float axis_factor = Vector3.Dot(vessel.ReferenceTransform.right, baseTransform.right) * fwd_airstream_factor;
+                float pitch_factor = axis_factor * Math.Sign(Vector3.Dot(world_com - baseTransform.position, vessel.ReferenceTransform.up));
+                if (was_deployed)
+                    prev_pitch_normdeflection = Common.Clampf(prev_pitch_normdeflection, Mathf.Abs(pitch_factor));
+                float new_pitch_action = pitch_input * pitch_factor;
+                prev_pitch_normdeflection = useExponentialSpeed
+                    ? Mathf.Lerp(prev_pitch_normdeflection, new_pitch_action, exp_spd_factor)
+                    : prev_pitch_normdeflection + Common.Clampf(new_pitch_action - prev_pitch_normdeflection, spd_factor * Math.Abs(axis_factor));
+            }
+
+            if (ignoreRoll)
+                prev_roll_normdeflection = 0.0f;
+            else
+            {
+                float axis_factor = Vector3.Dot(vessel.ReferenceTransform.up, baseTransform.up) * fwd_airstream_factor;
+                float roll_factor = axis_factor * Math.Sign(Vector3.Dot(vessel.ReferenceTransform.up,
+                    Vector3.Cross(world_com - baseTransform.position, baseTransform.forward)));
+                if (was_deployed)
+                    prev_roll_normdeflection = Common.Clampf(prev_roll_normdeflection, Mathf.Abs(roll_factor));
+                float new_roll_action = roll_input * roll_factor;
+                prev_roll_normdeflection = useExponentialSpeed
+                    ? Mathf.Lerp(prev_roll_normdeflection, new_roll_action, exp_spd_factor)
+                    : prev_roll_normdeflection + Common.Clampf(new_roll_action - prev_roll_normdeflection, spd_factor * axis_factor);
+            }
+
+            if (ignoreYaw)
+                prev_yaw_normdeflection = 0.0f;
+            else
+            {
+                float axis_factor = Vector3.Dot(vessel.ReferenceTransform.forward, baseTransform.right) * fwd_airstream_factor;
+                float yaw_factor = axis_factor * Math.Sign(Vector3.Dot(world_com - baseTransform.position, vessel.ReferenceTransform.up));
+                if (was_deployed)
+                    prev_yaw_normdeflection = Common.Clampf(prev_yaw_normdeflection, Mathf.Abs(yaw_factor));
+                float new_yaw_action = yaw_input * yaw_factor;
+                prev_yaw_normdeflection = useExponentialSpeed
+                    ? Mathf.Lerp(prev_yaw_normdeflection, new_yaw_action, exp_spd_factor)
+                    : prev_yaw_normdeflection + Common.Clampf(new_yaw_action - prev_yaw_normdeflection, spd_factor * Math.Abs(axis_factor));
+            }
+
+            was_deployed = false;
+
+            deflection = action = 0.01f * authorityLimiter * Common.Clampf(prev_pitch_normdeflection + prev_roll_normdeflection + prev_yaw_normdeflection, 1.0f);
+            ctrlSurface.localRotation = Quaternion.AngleAxis(deflection * ctrlSurfaceRange, Vector3.right) * neutral;
+        }
+    }
+}
